3a26e4164881949610717b3353dd11d71698cdcb
epidersis <32263760+epidersis@users.noreply.github.com>
2020-12-09 22:27:01 +0900

    added filter

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,38 @@
-let filterBtn = document.querySelector('.filter-button')
+let showBtn = $('.show-button')
+let resetBtn = $('.reset-button')
+let elements = $('.catalog-item')
+let rads = $('.catalog-checkbox-button')
 
-filterBtn.onclick = function() {
-  let rads = document.querySelector('.filter-group').querySelectorAll('.form-check-input');
-  console.log(rads);
-};
+let checkedAny = false;
+
+
+function hideShow(kinds=[]) {
+  elements.each(function() {
+    if ($(this).parent().hasClass('hide')) {
+      $(this).parent().removeClass('hide');
+    }
+  });
+
+  if (kinds.length == rads.length) {
+    return;
+  }
+
+  for (let i = 0; i < kinds.length; i++) {
+    elements.each(function() {
+      if ($(this).hasClass(kinds[i].dataset.name)) {
+        $(this).parent().addClass('hide');
+      }
+    });
+  }
+}
+
+
+showBtn.on('click', function() {
+  itemsToHide = $('.catalog-checkbox-button:checkbox:not(:checked)');
+  hideShow(itemsToHide);
+});
+
+resetBtn.on('click', function() {
+  hideShow();
+  rads.prop('checked', false);
+});
